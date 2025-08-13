ce225c0e8801c59f727d6ddd1fed2bc6e4e94fba
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-08-13 12:17:50 +0530

    deleted exe files

---
+++ DIV3A questions/falseAlarm.cpp
@@ -0,0 +1,20 @@
+/*
+    codeforces round 1029 (div 3)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        int arr[n];
+        for(int i =0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+    }
+    return 0;
+}
