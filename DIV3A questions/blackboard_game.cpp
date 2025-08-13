62dc5839cf1dcfc28916294f280dd333e6076379
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-08-13 10:53:08 +0530

    add blackboard game

---
+++ DIV3A questions/blackboard_game.cpp
@@ -0,0 +1,21 @@
+/*
+ codeforce round 1034(Div .3)
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%4!=0){
+            cout<<"alice"<<endl;
+        }
+        else{
+            cout<<"bob"<<endl;
+        }
+    }
+    return 0;
+} 
